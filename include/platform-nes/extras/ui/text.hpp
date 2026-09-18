e717cac701af7bff1cbb45628b3f5ac4d2f97f29
brette <brette.allen05@gmail.com>
2026-09-18 16:08:23 +0100

    repairs to philsophy of rtTextBox for ui

--- include/platform-nes/extras/ui/text.hpp
+++ include/platform-nes/extras/ui/text.hpp
@@ -68,41 +68,33 @@ namespace ui::text {
      *  always_inline needs to accept a body under LTO -- see ::AI's own
      *  comment in technology.hpp.
      */
-    template <Alignment align>
-    AI void Draw(const buffer<u8*>* rows, u16 address, vec2<u8> box);
+    AI void Draw(const buffer<u8*>* rows, u16 address, vec2<u8> box, Alignment align);
 
-    template <>
-    AI inline void Draw<Left>(const buffer<u8*>* rows, const u16 address, const vec2<u8> box) {
+
+    AI inline void Draw(const buffer<u8*>* rows, const u16 address, const vec2<u8> box, const Alignment align) {
         ppu::WriteFromBufferToNameTable(address, rows[0].addr, rows[0].size, 0);
         u16 rowAddr = address;
         for (u8 i = 1; i < box.y; i++) {
             rowAddr += video::viewport_tx();
-            ppu::WriteFromBufferToNameTable(rowAddr, rows[i].addr, rows[i].size, i);
+
+            u16 useAddr;
+            switch (align) {
+                case Left:
+                default:
+                    useAddr = rowAddr;
+
+                case Right:
+                    useAddr = rowAddr + (box.x - rows[i].size);
+
+                case Centre:
+                    useAddr = rowAddr + (box.x - rows[i].size) / 2;
+            }
+
+            ppu::WriteFromBufferToNameTable(useAddr, rows[i].addr, rows[i].size, i);
         }
     }
 
-    template <>
-    AI inline void Draw<Centre>(const buffer<u8*>* rows, const u16 address, const vec2<u8> box) {
-        u16 rowBase = address;
-        ppu::WriteFromBufferToNameTable(rowBase + (box.x - rows[0].size) / 2, rows[0].addr, rows[0].size, 0);
-        for (u8 i = 1; i < box.y; i++) {
-            rowBase += video::viewport_tx();
-            ppu::WriteFromBufferToNameTable(rowBase + (box.x - rows[i].size) / 2, rows[i].addr, rows[i].size, i);
-        }
-    }
-
-    template <>
-    AI inline void Draw<Right>(const buffer<u8*>* rows, const u16 address, const vec2<u8> box) {
-        u16 rowBase = address;
-        ppu::WriteFromBufferToNameTable(rowBase + (box.x - rows[0].size), rows[0].addr, rows[0].size, 0);
-        for (u8 i = 1; i < box.y; i++) {
-            rowBase += video::viewport_tx();
-            ppu::WriteFromBufferToNameTable(rowBase + (box.x - rows[i].size), rows[i].addr, rows[i].size, i);
-        }
-    }
-
-    template <Alignment align>
-    AI void Draw(const buffer<u8*>* chunks, const vec2<u16> pos, const u8 boxY) {
-        Draw<align>(chunks, ppu::CartesianToAddress(pos), boxY);
+    AI inline void Draw(const buffer<u8*>* chunks, const vec2<u16> pos, const vec2<u8> box, const Alignment align) {
+        Draw(chunks, ppu::CartesianToAddress(pos), box, align);
     }
 }
